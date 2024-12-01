03c70e5f61b0b274e64df8a52ae0de326924adcf
LILCODAH <rajkumar14ailawadi@gmail.com>
2024-12-02 02:20:52 +0530

    Delete q2.py

--- q2.py
+++
@@ -1,6 +0,0 @@
-x=int(input("enter a number: "))
-y=int(input("enter an another number: "))
-if x%y==0:
-    print(f"{x} is perfectly divisible by {y}")
-else:
-    print(f"{x} is not perfectly divisible by {y}")    
