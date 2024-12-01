6a487cb762b531dca785016a45ae93ea47572bb0
LILCODAH <rajkumar14ailawadi@gmail.com>
2024-12-02 02:20:44 +0530

    Delete q1.py

--- q1.py
+++
@@ -1,7 +0,0 @@
-m=int(input("enter a number: "))
-n=int(input("enter an another number: "))
-suma=0
-for num in range(m,n+1):
-    suma+=num
-    
-print(f"The sum of numbers between {m} and {n} is: {suma}")    
